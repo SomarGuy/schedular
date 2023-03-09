90077ae4748aebf65ef2c034cb5a7ab600c0bce6
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-09 15:30:41 -0500

    add story to Empty component to appointment stories

--- stories/index.js
+++ stories/index.js
@@ -152,4 +152,5 @@ backgrounds: [{ name: "white", value: "#fff", default: true }]
 })
 .add("Appointment", () => <Appointment />)
 .add("Appointment with Time", () => <Appointment time="12pm" />)
-.add("Header", () => <Header time="12pm" />);
+.add("Header", () => <Header time="12pm" />)
+.add("Empty", () => <Empty onAdd={action("onAdd")}/>);
