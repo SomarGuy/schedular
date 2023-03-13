04c67806c245ebcc461385563127e1190eb0b29e
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-13 00:37:46 -0400

    updated with axios and useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,24 +1,8 @@
 import "components/Application.scss";
 import DayList from "./DayList";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Appointment from "./Appointment";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
 
 const appointments = {
   "1": {
@@ -61,6 +45,14 @@ const appointments = {
 
 export default function Application(props) {
   const [value, onChange] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      setDays(response.data);
+    });
+  }, []);
+  
 
   return (
 
