8c55b51487766795e04b8ff76e2f9ac0e46ea03a
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-14 15:54:40 -0400

    Update the Appointment component with the appropriate conditional logic to handle the transition to CREATE.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,26 +4,28 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY
-  );
+const { mode, transition, back } = useVisualMode(
+props.interview ? SHOW : EMPTY
+);
 
-  return (
-    <article className="appointment">
-      <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && (
-        <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-        />
-      )}
-    </article>
-  );
+return (
+<article className="appointment">
+<Header time={props.time} />
+{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+{mode === SHOW && (
+<Show
+       student={props.interview.student}
+       interviewer={props.interview.interviewer}
+     />
+)}
+{mode === CREATE && <Form interviewers={[]} onCancel={back} />}
+</article>
+);
 }
