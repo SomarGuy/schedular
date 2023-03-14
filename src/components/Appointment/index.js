33bb796c88d30c91ce36f4be0e9dfe74412acd54
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-14 15:51:41 -0400

    Add a new constant for the CREATE mode similar to the ones for SHOW and EMPTY.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
