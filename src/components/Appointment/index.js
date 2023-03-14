2f0e1120006410e070f18bec111381042bdefc2f
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-14 15:47:09 -0400

    Import useVisualMode into the Appointment component.
    Add the mode constants above to the Appointment component.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,9 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
   return (
