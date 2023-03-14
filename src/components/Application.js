f1aaa5c5de1d38f9e630d6efafdf95a0b252514b
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-13 21:15:01 -0400

    Update the code in our Application component to match (or be similar to) the snippet below.

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,7 +11,6 @@ export default function Application(props) {
     days: [],
     appointments: {},
     interviewers: {}
-
   });
 
   const setDay = day => setState({ ...state, day });
@@ -21,11 +20,29 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then(all => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }));
     });
   }, []);
+
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -50,9 +67,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => (
-          <Appointment key={appointment.id} {...appointment} />
-        ))}
+        {schedule}
       </section>
     </main>
   );
