378d4a5d66539796bce1cd69db8bbbadc12cecaa
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-14 15:15:00 -0400

    Add the SECOND constant under the FIRST constant in our test file.

--- src/hooks/__test__/useVisualMode.test.js
+++ src/hooks/__test__/useVisualMode.test.js
@@ -3,6 +3,7 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
+const SECOND = "SECOND";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
