6865c8e026a5825dd958845b45a98b00d5c4bbdb
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-28 17:09:18 -0400

    implement should edit an interview test in cypress

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,14 +1,39 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
-    cy.request("GET", "/api/debug/reset")
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
-    .first()
-    .click();
+      .first()
+      .click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
 
+    // Add the commands to verify student and interviewer names
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get(".appointment__card--show")
+      .first()
+      .trigger("mouseover");
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
