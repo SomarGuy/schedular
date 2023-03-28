bdb89b3566f912e40072d08738088eb55be399ef
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-03-28 16:57:30 -0400

    Update the test to check for the day-list__item--selected class instead of the background-color property.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,9 +3,9 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 
-  it("should navigate to Tuesday and check the background color", () => {
+  it("should navigate to Tuesday and check for the day-list__item--selected class", () => {
     cy.contains("li", "Tuesday")
       .click()
-      .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .should("have.class", "day-list__item--selected");
   });
 });
